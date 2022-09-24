dce436b7d025f083184218f01307dc08da57e34c
Seojin Oh <oseojin5137@gmail.com>
2022-09-24 15:18:54 +0900

    Add Draw Fill

--- app.js
+++ app.js
@@ -1,39 +1,91 @@
+const modeBtn = document.getElementById("mode-btn");
+const colorOptions = Array.from(
+    document.getElementsByClassName("color-option")
+    );
+const lineWidth = document.getElementById("line-width");
+const color = document.getElementById("color");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
+let isPainting = false;
+let isFilling = false;
 
-const colors = [
-    "#1abc9c",
-    "#2ecc71",
-    "#3498db",
-    "#9b59b6",
-    "#34495e",
-    "#16a085",
-    "#27ae60",
-    "#2980b9",
-    "#8e44ad",
-    "#2c3e50",
-    "#f1c40f",
-    "#e67e22",
-    "#e74c3c",
-    "#ecf0f1",
-    "#95a5a6",
-    "#f39c12",
-    "#d35400",
-    "#c0392b",
-    "#bdc3c7",
-    "#7f8c8d"
-]
-
-function onClick(event) {
+function onMove(event)
+{
+    if(isPainting && !isFilling)
+    {
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
     ctx.beginPath();
-    ctx.moveTo(0, 0);
-    const color = colors[Math.floor(Math.random() * colors.length)];
-    ctx.strokeStyle = color;
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
+    ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-canvas.addEventListener("mousemove", onClick);
+function startPainting(event)
+{
+    isPainting = true;
+}
+
+function cancelPainting(event)
+{
+    isPainting = false;
+}
+
+function onLineWidthChange(event)
+{
+    ctx.lineWidth = event.target.value;
+}
+
+function onColorChange(event)
+{
+    ctx.strokeStyle = event.target.value;
+    ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event)
+{
+    const colorValue = event.target.dataset.color;
+    console.dir(event.target.dataset.color);
+    ctx.strokeStyle = colorValue;
+    ctx.fillStyle = colorValue;
+    color.value = colorValue;
+}
+
+function onModeClick()
+{
+    if(isFilling)
+    {
+        isFilling = false;
+        modeBtn.innerText = "Draw";
+    }
+    else
+    {
+        isFilling = true;
+        modeBtn.innerText = "Fill";
+    }
+}
+
+function onCanvasClick()
+{
+    if(isFilling)
+    {
+        ctx.fillRect(0, 0, 800, 800);
+    }
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+
+canvas.addEventListener("click", onCanvasClick);
+
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+
+colorOptions.forEach(color => color.addEventListener("click", onColorClick));
+
+modeBtn.addEventListener("click", onModeClick);
